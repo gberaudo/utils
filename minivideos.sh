13060bdf5dbb31dc22db599ce2dde12936c5294c
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2017-11-25 23:43:11 +0100

    Fix suffix and limit rate changes
    - add a single mp4 suffix;
    - only change rate of 50p videos

--- minivideos.sh
+++ minivideos.sh
@@ -1,7 +1,10 @@
-> videos.html
-for i in *.MTS *.mp4;
+#/bin/bash -e
+
+input=$*
+: ${input:=*.MTS *.mp4 *.MP4}
+for i in $input;
 do
-  out="mini_$i.mp4"
-  ffmpeg -i $i -c:v libx264 -preset slow -crf 22 -vf scale=iw/2:-1 -r 25 -ac 2 -strict -2 -c:a aac $out
-  echo '<video width="960" height="540" controls><source src="'$out'">Your browser does not support the video tag.</video>' >> videos.html
+  out="mini_`basename $i| sed 's/\.mp4$//I'`.mp4"
+  ffprobe -show_streams -select_streams v -hide_banner $i 2>/dev/null| grep r_frame_rate | grep '50/1' && RATIO="-r 25"
+  echo ffmpeg -i $i -c:v libx264 -preset slow -crf 22 -vf scale=iw/2:-1 $RATIO -ac 2 -strict -2 -c:a aac $out
 done
